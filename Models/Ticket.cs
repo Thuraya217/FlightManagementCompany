1131b646b8e2411b22a1182d90f15e6ec12bb5d3
Thuraiya <3eya151@gmail.com>
2025-08-12 16:31:12 +0400

    create Ticket calss

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Models
 {
-    class Ticket
+    public class Ticket
     {
+        [Key]
+        public int TicketId { get; set; }
+        public string SeatNumber { get; set; }
+        public decimal Fare { get; set; }
+        public bool CheckedIn {  get; set; }
+
+        // Foreign key to the Booking entity
+        [ForeignKey("Booking")]
+        public int BookingId { get; set; }
+        public Booking Booking { get; set; } // Navigation property to the Booking entity
+
+        // Foreign key to the Passenger entity
+        [ForeignKey("Flight")]
+        public int FlightId { get; set; } // Foreign key to the Flight entity
+        public Flight Flight { get; set; } // Navigation property to the Flight entity
+
+        public ICollection<Baggage> Baggages { get; set; } // Collection of baggage associated with the ticket
     }
 }
