1afb1b370a4e186182ad347d7a19b0be37a675f6
Thuraiya <3eya151@gmail.com>
2025-08-12 16:29:20 +0400

    create Baggage calss

--- Models/Baggage.cs
+++ Models/Baggage.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Models
 {
-    class Baggage
+    public class Baggage
     {
+        [Key]
+        public int BaggageId { get; set; }
+
+        [Required]
+        public decimal WeightKg { get; set; } // Weight of the baggage in kilograms
+
+        [Required]
+        public string TagNumber { get; set; } // Unique tag number for the baggage
+
+        [ForeignKey("Ticket")]
+        public int TicketId { get; set; }
+        public Ticket Ticket { get; set; } // Navigation property to the Ticket entity
     }
 }
