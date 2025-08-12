331210a7a78444d91e4776af21a849fc1f496c18
Thuraiya <3eya151@gmail.com>
2025-08-12 16:29:50 +0400

    create Crew Member calss

--- Models/CrewMember.cs
+++ Models/CrewMember.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Models
 {
-    class CrewMember
+    // Represents the Role of a crew member in a flight
+    public enum CrewRole
     {
+        Pilot,
+        CoPilot,
+        FlightAttendant
+    }
+    public class CrewMember
+    {
+        [Key]
+        public int CrewId { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(CrewRole))]
+        public CrewRole Role { get; set; } // e.g., Pilot, CoPilot, FlightAttendant
+
+        // ? can be null
+        public string? LicenseNumber { get; set; } // For pilots, the license number
+
+        public virtual ICollection<FlightCrew> FlightCrews { get; set; } // Collection of flight crew assignments for this crew member
     }
 }
