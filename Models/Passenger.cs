5e5ac4c31e74654d6e672be2a9d330ae3135d468
Thuraiya <3eya151@gmail.com>
2025-08-12 16:30:48 +0400

    create Passenger calss

--- Models/Passenger.cs
+++ Models/Passenger.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Models
 {
-    class Passenger
+    [Index(nameof(PassportNumber), IsUnique = true)]
+
+    public class Passenger
     {
+        [Key]
+        public int PassengerId { get; set; }
+
+        [Required]  
+        public string FullName { get; set; } // Full name of the passenger
+
+        [Required]
+        public string PassportNumber { get; set; } // Unique passport number
+        public string Nationality { get; set; }
+        public DateTime DateOfBirth { get; set; } // Date of birth of the passenger
+
+        public ICollection<Booking> Bookings { get; set; } // Collection of bookings associated with the passenger
     }
 }
