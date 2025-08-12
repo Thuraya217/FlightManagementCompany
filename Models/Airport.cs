04bf35a021b65c2426fff1de8d3ecfa0a44a3406
Thuraiya <3eya151@gmail.com>
2025-08-12 08:36:26 +0400

    Add project files.

---
+++ Models/Airport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany.Models
+{
+    public class Airport
+    {
+        public int AirportId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string IATA { get; set; } // International Air Transport Association code
+        public string ICAO { get; set; } // International Civil Aviation Organization code
+        public string TimeZone { get; set; } // Time zone of the airport
+    }
+}
