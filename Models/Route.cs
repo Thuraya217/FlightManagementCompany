f5a5c8b1d6c8c2ca9d5e96c3e58d5d8dc0179840
Thuraiya <3eya151@gmail.com>
2025-08-12 16:30:57 +0400

    create Route calss

--- Models/Route.cs
+++ Models/Route.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Models
 {
-    class Route
+
+    public class Route
     {
+        [Key]
+        public int RouteId { get; set; } // Unique identifier for the route
+        public int DistanceKm { get; set; } // Distance in kilometers 
+
+        // FK for Origin Airport
+        [ForeignKey("OriginAirport")]
+        public int OriginAirportId { get; set; }
+        public Airport OriginAirport { get; set; }
+
+        // FK for Destination Airport
+        [ForeignKey("DestinationAirport")]
+        public int DestinationAirportId { get; set; }
+        public Airport DestinationAirport { get; set; }
+
+        public ICollection<Flight> Flights { get; set; } // Collection of flights associated with the route
     }
 }
