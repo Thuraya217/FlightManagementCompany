8050ece5d7d334a893cad9cd0041630b40458937
Thuraiya <3eya151@gmail.com>
2025-08-12 16:29:07 +0400

    create Airport calss

--- Models/Airport.cs
+++ Models/Airport.cs
@@ -1,19 +1,30 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Models
 {
+    [Index(nameof(IATA), IsUnique = true)]
+
     public class Airport
     {
+        [Key]
         public int AirportId { get; set; }
+        [Required]
         public string Name { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+
+        [Required]
+        [MaxLength(3)]
         public string IATA { get; set; } // International Air Transport Association code
-        public string ICAO { get; set; } // International Civil Aviation Organization code
         public string TimeZone { get; set; } // Time zone of the airport
+
+        public ICollection<Route> OriginRoutes { get; set; } // start routes from this airport
+        public ICollection<Route> DestinationRoutes { get; set; } // end routes at this airport
     }
 }
